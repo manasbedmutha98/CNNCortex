1a41d6d3c6a9f4b8f207ccbbfd01a9550ca81349
GlintIITGn <35600805+GlintIITGn@users.noreply.github.com>
2019-02-19 23:06:19 +0530

    sum of all elements in the array implemented

--- CNNCortex.h
+++ CNNCortex.h
@@ -2,22 +2,35 @@
 #define CNNCortex_H_  
 //the above construct is to ensure that this header file contents are only added once
 
+#include<stdlib.h>
+
+
 /*Insert Documentation*/
-int * matrixPacker(int * inputMatrix, int numberOfRows, int numberOfColumns , int height ){
+int * matrixPacker(int * inputMatrix, int height, int numberOfRows, int numberOfColumns ){
 	int * packedMatrix;
-	//ToDo: logic here
+	int sizeOfArray=(numberOfRows*numberOfColumns*height + 3)*sizeof(int);
+	packedMatrix=malloc(sizeOfArray);
+	packedMatrix[0]=height;
+	packedMatrix[1]=numberOfRows;
+	packedMatrix[2]=numberOfColumns;
+	memcpy((void *)(packedMatrix+3), (const void *) inputMatrix,(unsigned) sizeOfArray);
 	
 	return packedMatrix;
 }
 
 /*Insert Documentation*/
-void matrixShape(int * inputMatrix, int * outShape, int * outShapeLength){
-
+void matrixShape(int * inputMatrix, int * outShape){
+	outShape=malloc(sizeof(int)*3);
+	
+	outShape[0]=inputMatrix[0];
+	outShape[1]=inputMatrix[1];
+	outShape[2]=inputMatrix[2];
+	
 }
 
 /*Insert Documentation*/
 int matrixLength(int * inputMatrix){
-	int length;
+	int length=inputMatrix[2];
 	//ToDo: Logic here
 	
 	return length;
@@ -25,13 +38,50 @@ int matrixLength(int * inputMatrix){
 
 /*Insert Documentation*/
 int matrixSum(int * inputMatrix){
-	int sum;
+	int sum=0;
+	int height=inputMatrix[0];
+	int rows=inputMatrix[1];
+	int columns=inputMatrix[2];
 	
+	int length=height; //*rows*columns;
+	
+	__asm{ 
+		MULS length,rows
+		MULS length,columns
+	}
+	
+	int r0=(int)(inputMatrix+3);
+	int counter =0;
+	int readInFromMemory=0;
+	__asm{		
+		LDR sum, [r0]
+		loop1:	
+		ADDS counter, 0x1
+		CMP counter, length
+		BEQ exit1
+		ADDS r0, 0x4
+		LDR readInFromMemory, [r0]
+		ADDS sum, readInFromMemory
+		B loop1
+		exit1:
+	}
 	return sum;
 }
 
 /*Insert Documentation*/
 void matrixElementwiseMul(int * result, int * inputMatrixOne, int * inputMatrixTwo){
+	//ToDo: add sanity check to the size of the matrix
+	int height=inputMatrix[0];
+	int rows=inputMatrix[1];
+	int columns=inputMatrix[2];
+	
+	int length=height; //*rows*columns;
+	
+	__asm{ 
+		MULS length,rows
+		MULS length,columns
+	}
+	
 	
 }
 
--- source.c
+++ source.c
@@ -2,10 +2,37 @@
 #include <MKL25Z4.h>
 #include "CNNCortex.h"
 
-
-
-
 int main(){
+	
+	int myArray[3][10][10];
+	int i,j,k; 
+	int * packedMyArray;
+	int myArrayLength;
+	int myArraySum;
+	int * myArrayElementWiseProduct;
+	
+	int * shapeOfMyMatrix;
+	
+	for( i=0;i<3;i++){
+		for(j=0;j<10;j++){
+			for(k=0;k<10;k++){
+				myArray[i][j][k]=j;
+			}
+		}
+	}
+	
+	packedMyArray = matrixPacker((int *)myArray,3,10,10);
+	
+	matrixShape(packedMyArray,shapeOfMyMatrix);
+	
+	myArrayLength=matrixLength(packedMyArray);
+	
+	myArraySum=matrixSum(packedMyArray);
+	
+	matrixElementwiseMul(myArrayElementWiseProduct,packedMyArray,packedMyArray);
+	
+	
+	
 	
 	
 	return 1;
